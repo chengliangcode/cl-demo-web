3651576b690a1cb2f91c9c7ae4e90420d7d6ba5f
CL <CL@Conpany.com>
2020-03-05 16:10:14 +0800

    添加ant-vue,侧边菜单

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+   <cl-side-bar/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ClSideBar from './components/ClSidebar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ClSideBar
   }
 }
 </script>
@@ -23,6 +25,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: #ccc;
+  width: 100%;
+  height: 100%;
+}
+*{
+    margin: 0;
+    padding: 0;
+}
+html, body{
+    width: 100%;
+    height: 100%;
 }
 </style>
